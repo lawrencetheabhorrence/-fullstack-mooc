830d1018001eb382185700cc53baa9de5bd5f49f
lawrencetheabhorrence <tripwireplease@gmail.com>
2020-09-30 06:47:34 +0800

    added buttons and statistics

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Score = ({name, score}) =>{
+	return (<><p>{name} {score}</p></>)
+}
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
@@ -9,7 +13,14 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <button onClick={()=>setGood(good + 1)}>good</button>
+      <button onClick={()=>setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={()=>setBad(bad + 1)}>bad</button>
+      <h1>statistics</h1>
+      <Score name="good" score={good}/>
+      <Score name="neutral" score={neutral}/>
+      <Score name="bad" score={bad}/>
     </div>
   )
 }
