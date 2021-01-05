ccd1b30e4f8dd4fc38bc75d3a9ca7de2b3b3a312
EVC Chronicles Admin <tripwireplease@gmail.com>
2021-01-06 00:04:35 +0800

    ex 6.19 - 6.21

--- redux-anecdotes/src/components/AnecdoteList.js
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -1,14 +1,9 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 import { setNotification } from '../reducers/notifReducer'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
 
-const Anecdote = ({id, content, votes}) => {
-  const dispatch = useDispatch()
-  const vote = (id, content) => {
-    dispatch(voteAnecdote(id))
-    dispatch(setNotification(content, 10000))
-  }
+const Anecdote = ({id, content, votes, vote}) => {
 
   return (
     <div>
@@ -23,20 +18,36 @@ const Anecdote = ({id, content, votes}) => {
   )
 }
 
-const AnecdoteList = () => {
-  const anecdotes = useSelector(({ filter, anecdotes }) => {
-    if (filter === ''){
-      return anecdotes
-    }
-    else {
-      return anecdotes.filter(anecdote => anecdote.content.toLowerCase().includes(filter.toLowerCase()))
-    }
-  })
+const AnecdoteList = (props) => {
+  const vote = (id, content) => {
+    props.voteAnecdote(id)
+    props.setNotification(content, 50000)
+  }
+
   return (
     <div>
-      {anecdotes.map(anecdote => <Anecdote key={anecdote.id} id={anecdote.id} content={anecdote.content} votes={anecdote.votes} />)}
+      {props.anecdotes.map(anecdote => <Anecdote key={anecdote.id} id={anecdote.id} content={anecdote.content} votes={anecdote.votes} vote={() => vote(anecdote.id, anecdote.content)}/>)}
     </div>
   )
 }
 
-export default AnecdoteList
+const mapStateToProps = (state) => {
+  if ( state.filter === '' ) {
+    return {
+      anecdotes: state.anecdotes
+    }
+  }
+  else {
+    return {
+      anecdotes: state.anecdotes.filter(anecdote => anecdote.content.toLowerCase().includes(state.filter.toLowerCase()))
+    }
+  }
+}
+
+const mapDispatchToProps = {
+  voteAnecdote, setNotification
+}
+
+const ConnectedList = connect(mapStateToProps, mapDispatchToProps)(AnecdoteList)
+
+export default ConnectedList
--- redux-anecdotes/src/components/Filter.js
+++ redux-anecdotes/src/components/Filter.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 import { setFilter } from '../reducers/filterReducer'
 
-const Filter = () => {
-  const dispatch = useDispatch()
+const Filter = (props) => {
   const handleChange = (event) => {
     const content = event.target.value
-    dispatch(setFilter(content))
+    props.setFilter(content)
   }
   const style = { marginBottom: 10 }
 
@@ -17,4 +16,8 @@ const Filter = () => {
   )
 }
 
-export default Filter
+const mapDispatchToProps = { setFilter, }
+
+const mapStateToProps = (state) => {}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Filter)
